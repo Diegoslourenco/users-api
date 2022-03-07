a190cc3b2eea46bc0db292bc1172ad8c04bb0f90
Diegoslourenco <diego.lourenco15@gmail.com>
2022-03-07 01:08:34 -0300

    add app components

---
+++ frontend/usersApp/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { APP_BASE_HREF } from '@angular/common';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './_components/menu/menu.component';
+import { HomeComponent } from './_components/home/home.component';
+import { ProfileComponent } from './_components/profile/profile.component';
+
+import { ProfileService } from './_services/profile.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    ProfileComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+  ],
+  providers: [
+    ProfileService,
+    { provide: APP_BASE_HREF, useValue: '/' }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
