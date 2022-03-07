a50a3f1672815299edb8742080501f46e8ec6dbd
Diegoslourenco <diego.lourenco15@gmail.com>
2022-03-07 01:45:22 -0300

    update app module and routing for new profile separated module for frontend

--- frontend/usersApp/src/app/app-routing.module.ts
+++ frontend/usersApp/src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from "./_components/home/home.component";
-import { ProfileComponent } from "./_components/profile/profile.component";
+
+const profilesModule = () => import('./profiles/profiles.module').then(x => x.ProfilesModule);
 
 const usersModule = () => import('./users/users.module').then(x => x.UsersModule);
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'profiles', component: ProfileComponent },
+  { path: 'profiles', loadChildren: profilesModule },
   { path: 'users', loadChildren: usersModule },
 ];
 
